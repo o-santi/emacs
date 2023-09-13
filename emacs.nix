f0a1087f251b01888acfd5c8b73495f6dc02962e
Leonardo Santiago <leonardo@mixrank.com>
2023-09-13 19:04:00 -0300

    remove pylint + flycheck, use pylsp with ruff plugin

--- emacs.nix
+++ emacs.nix
@@ -5,9 +5,34 @@ let
   #  (pkgs.nerdfonts.override { fonts = ["Iosevka"]; })
 
   ];
+  # taken from
+  # https://github.com/NixOS/nixpkgs/issues/229337
+  python = pkgs.python3.override {
+    packageOverrides = self: super: {
+      python-lsp-server = super.python-lsp-server.overridePythonAttrs (oldAttrs: {
+        propagatedBuildInputs = oldAttrs.propagatedBuildInputs or [] ++ [
+          self.python-lsp-ruff
+        ];
+      });
+      python-lsp-ruff = super.python-lsp-ruff.overridePythonAttrs (oldAttrs: {
+        postPatch = oldAttrs.postPatch or '''' + ''
+          sed -i '/python-lsp-server/d' pyproject.toml
+        '';
+      
+        nativeBuildInputs = with super; [
+          setuptools          
+        ];
+
+        propagatedBuildInputs = with super; [
+          lsprotocol
+          tomli
+        ];
+        doCheck = false;
+      });
+    };
+  };
   outside-emacs = [
-    pkgs.python311Packages.python-lsp-server
-    pkgs.pylint
+    python.pkgs.python-lsp-server
     pkgs.nil
   ];
   org-tangle-elisp-blocks = (pkgs.callPackage ./org.nix {inherit pkgs from-elisp;}).org-tangle ({ language, flags } :
